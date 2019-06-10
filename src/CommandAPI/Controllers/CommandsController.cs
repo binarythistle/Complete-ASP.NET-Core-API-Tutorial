019c1027089a0e8d1e9c7ac0491c3f6d18db70a8
les jackson <les.jackson@binarythistle.com>
2019-06-10 17:34:33 +1000

    Azure Pipelines File

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,7 +1,8 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using CommandAPI.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using CommandAPI.Models;
 
 namespace CommandAPI.Controllers
 {
@@ -19,13 +20,76 @@ namespace CommandAPI.Controllers
             _hostEnv = hostEnv;
         }
 
+        //GET:      api/commands
         [HttpGet]
         public ActionResult<IEnumerable<Command>> GetCommandItems()
         {
-            Response.Headers.Add("Environment", _hostEnv.EnvironmentName);
+            if (Response != null)
+                Response.Headers.Add("Environment", _hostEnv.EnvironmentName);
 
             return _context.CommandItems;
         }
+
+        //GET:      api/commands/Id
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandItem(int id)
+        {
+            var commandItem = _context.CommandItems.Find(id);
+
+            if (commandItem == null)
+                return NotFound();
+
+
+            return commandItem;
+        }
+
+        //POST:     api/commands
+        [HttpPost]
+        public ActionResult<Command> PostCommandItem(Command command)
+        {
+            _context.CommandItems.Add(command);
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction("GetCommandItem", new Command { Id = command.Id }, command);
+        }
+
+        //PUT:     api/commands/n
+        [HttpPut("{id}")]
+        public ActionResult PutCommandItem(int id, Command command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(command).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Command> DeleteCommandItem(int id)
+        {
+            var commandItem = _context.CommandItems.Find(id);
+
+            if (commandItem == null)
+                return NotFound();
+
+            _context.CommandItems.Remove(commandItem);
+            _context.SaveChanges();
+
+            return commandItem;
+        }
+
 
     }
 }
